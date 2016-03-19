70349541c4e0c98db2c2df44469875f7c33e7ee7
fredrikkivi <fredrik.kivi@ugli.se>
2016-03-19 06:52:32 +0100

    ClobReader encoding bug fixed

--- src/main/java/se/ugli/habanero/j/internal/ClobReader.java
+++ src/main/java/se/ugli/habanero/j/internal/ClobReader.java
@@ -1,6 +1,6 @@
 package se.ugli.habanero.j.internal;
 
-import java.io.InputStream;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.sql.Clob;
 import java.sql.SQLException;
@@ -11,17 +11,19 @@ import se.ugli.habanero.j.HabaneroException;
 
 public class ClobReader {
 
-	public static String read(final Clob clob) {
-		InputStream inputStream = null;
-		try {
-			inputStream = clob.getAsciiStream();
-			final Charset charset = HabaneroProperties.apply().getCharset();
-			return CopyCommand.apply().copyToString(inputStream, charset);
-		} catch (final SQLException e) {
-			throw new HabaneroException(e);
-		} finally {
-			CloseCommand.execute(inputStream);
-		}
-	}
+    public static String read(final Clob clob) {
+        Reader reader = null;
+        try {
+            reader = clob.getCharacterStream();
+            final Charset charset = HabaneroProperties.apply().getCharset();
+            return CopyCommand.apply().copyToString(reader, charset, charset);
+        }
+        catch (final SQLException e) {
+            throw new HabaneroException(e);
+        }
+        finally {
+            CloseCommand.execute(reader);
+        }
+    }
 
 }
